3d74d67dd1ab128d113e675220b145643381b4bd
Ting Lew <ting.lew@gmail.com>
2015-07-17 23:10:11 -0400

    first commit

---
+++ .meteor/server/app.js
@@ -0,0 +1,17 @@
+// Meteor.startup(function () {
+//   if (QuestionList.find().count() === 0) {
+//     var names = [
+//       ["What is a city that sounds like Natalie Portman's last name?",
+//       "portland"],
+//       ["What is the New York park that starts with a P",
+//       "prospect park"]
+//     ];
+
+//     _.each(names, function (name) {
+//       QuestionList.insert({
+//         question: name[0],
+//         answer: name[1]
+//       });
+//     });
+//   }
+// });
